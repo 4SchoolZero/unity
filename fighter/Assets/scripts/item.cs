7c4d48fe22fdc0f5f307464d10e82225e8800767
Tijmen <34179@ma-web.nl>
2022-03-31 11:33:00 +0200

    movement fixed, audio change

--- fighter/Assets/scripts/item.cs
+++ fighter/Assets/scripts/item.cs
@@ -5,23 +5,18 @@ using UnityEngine;
 public class item : MonoBehaviour
 {
     public GameObject prefab;
-    public AudioSource sound;
     // Start is called before the first frame update
     void Start()
     {
-        AudioSource sound = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
     public void OnTriggerEnter(Collider other){
-            sound.Play();
             stamina.instance.UseStamina(-100);
             Destroy(gameObject);
             Instantiate(prefab, transform.position, Quaternion.identity);
-            
     }
 }
--- fighter/Assets/scripts/movement.cs
+++ fighter/Assets/scripts/movement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class movement : MonoBehaviour
 {
     public Rigidbody rb;
+    public bool isgrounded;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,9 +21,19 @@ public class movement : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.A)){
             rb.velocity = new Vector3(-5, 0, 0);
         }
-        if (Input.GetKeyDown(KeyCode.Space)){
+        if (Input.GetKeyDown(KeyCode.Space)&& isgrounded == true){
             stamina.instance.UseStamina(20);
             rb.velocity = new Vector3(0, 5, 0);
+            isgrounded = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "ground")
+        {
+            isgrounded = true;
+            Debug.Log("werkt");
         }
     }
 }
